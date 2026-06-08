8a55a3b431fc6c6367dd06b5ef7e09cc8c98a07b
Yuvraj Paliwal <realyuvraj100@gmail.com>
2026-06-08 10:18:49 +0530

    practice: update 2026-06-08 10:18:49

--- Practice.py
+++ Practice.py
@@ -1,26 +1,54 @@
-# # 1. Write a python program to add two numbers.
+# # 10. Write a program to format the following letter using escape sequence characters
 
-# # a = int(input("Enter Number 1 "))
-# # b = int(input("Enter Number 2 "))
-# a = 10
-# b = 20
+# letter = "Hey, I am Yuvraj"
 
-# print("The sum of a and b is ", a+b)
+# print("Hey,\nI am \"Yuvraj\"")
 
-# # 1. Write a python program to add two numbers.
 
-# # a = int(input("Enter Number 1 "))
-# # b = int(input("Enter Number 2 "))
-# a = 43
-# b = 5
+# # 9. Write a program to detect double space in a string and Replace the double space  with single spaces.
+
+
+# str = "Double  Trouble"
+
+# rep = str.replace("  ", " ")
+
+# print(rep)
+
+
+# # 8.  Write a program to fill in a letter template given below with name and date.
+
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+# print(letter.replace("<|Name|>", input("Enter ur name : ")).replace("<|Date|>", "11 June" ))
+
+
+# # 7. Write a python program to display a user entered name followed by Good Afternoon using
+# # input() function
+
+# Name = input("Enter your name ")
+# print("Good Morning,", Name)
+
+
+
+
+# # 6. Write a python program to calculate the square of a number entered by the user.
+
+# num = int(input("Enter the number : "))
+
+# print(num*num)
+
+
+# # 5. Write a python program to find an average of two numbers entered by the user.
+
+# a = 22
+# b = 10
+
+# print((a+b)/2)
 
 # print("The remainder of a divided by b is ", a%b)
-
-
-# # 3. Check the type of variable assigned using input() function.
-
-# a = input("Enter something ")
-# print("The type of a is ", type(a))
 
 
 # # 4. Use comparison operator to find out whether ‘a’ given variable is greater than ‘b’ or not.
@@ -31,16 +59,25 @@
 # print(a>b)
 
 
-# # 5. Write a python program to find an average of two numbers entered by the user.
+# # 3. Check the type of variable assigned using input() function.
 
-# a = 22
-# b = 10
-
-# print(a+b/2)
+# a = input("Enter something ")
+# print("The type of a is ", type(a))
 
 
-# # 6. Write a python program to calculate the square of a number entered by the user.
+# # 2. Write a python program to add two numbers.
 
-# num = int(input("Enter the number : "))
+# # a = int(input("Enter Number 1 "))
+# # b = int(input("Enter Number 2 "))
+# a = 43
+# b = 5
 
-# print(num*num)
+
+# # 1. Write a python program to add two numbers.
+
+# # a = int(input("Enter Number 1 "))
+# # b = int(input("Enter Number 2 "))
+# a = 10
+# b = 20
+
+# print("The sum of a and b is ", a+b)
