32c195269d23a57acabf7c30bd8f79a030c2acf5
Yuvraj Paliwal <realyuvraj100@gmail.com>
2026-06-08 08:59:40 +0530

    practice: update 2026-06-08 08:59:40

--- Practice.py
+++ Practice.py
@@ -1,15 +1,46 @@
-# 1. Write a python program to add two numbers.
+# # 1. Write a python program to add two numbers.
 
-# a = int(input("Enter Number 1 "))
-# b = int(input("Enter Number 2 "))
-a = 10
-b = 20
+# # a = int(input("Enter Number 1 "))
+# # b = int(input("Enter Number 2 "))
+# a = 10
+# b = 20
 
-print("The result is ", a+b)
+# print("The sum of a and b is ", a+b)
 
-# 1. Write a python program to add two numbers.
+# # 1. Write a python program to add two numbers.
 
-a = int(input("Enter Number 1 "))
-b = int(input("Enter Number 2 "))
+# # a = int(input("Enter Number 1 "))
+# # b = int(input("Enter Number 2 "))
+# a = 43
+# b = 5
 
-print("The result is ", a%b)
+# print("The remainder of a divided by b is ", a%b)
+
+
+# # 3. Check the type of variable assigned using input() function.
+
+# a = input("Enter something ")
+# print("The type of a is ", type(a))
+
+
+# # 4. Use comparison operator to find out whether ‘a’ given variable is greater than ‘b’ or not.
+# # Take a = 34 and b = 80
+
+# a = 34
+# b = 80
+# print(a>b)
+
+
+# # 5. Write a python program to find an average of two numbers entered by the user.
+
+# a = 22
+# b = 10
+
+# print(a+b/2)
+
+
+# # 6. Write a python program to calculate the square of a number entered by the user.
+
+# num = int(input("Enter the number : "))
+
+# print(num*num)
