4be87dbb8e78dd4330527c4680e18a32085f9d4c
Yuvraj Paliwal <realyuvraj100@gmail.com>
2026-06-08 08:41:46 +0530

    practice: update 2026-06-08 08:41:46

--- Practice.py
+++ Practice.py
@@ -1,6 +1,15 @@
 # 1. Write a python program to add two numbers.
 
+# a = int(input("Enter Number 1 "))
+# b = int(input("Enter Number 2 "))
+a = 10
+b = 20
+
+print("The result is ", a+b)
+
+# 1. Write a python program to add two numbers.
+
 a = int(input("Enter Number 1 "))
 b = int(input("Enter Number 2 "))
 
-print("The result is ", a+b)
+print("The result is ", a%b)
