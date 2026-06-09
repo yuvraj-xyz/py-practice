bb38557dc8d33c64739980d8ed06b2078e111b83
Yuvraj Paliwal <realyuvraj100@gmail.com>
2026-06-09 14:38:45 +0530

    push this too

--- Practice.py
+++ Practice.py
@@ -1,8 +1,36 @@
-# 18. Can we have a set with 18 (int) and '18' (str) as a value in it?
 
-s = (18, "18")
 
-print(s, type(s))
+
+
+
+# 20. Create an empty dictionary. Allow 4 friends to enter their favorite language as value and
+# use key as their names. Assume that the names are unique.
+
+dict = {}
+
+for i in range (0, 4):
+    dict.update({(input("Enter Your Name : ")):input("Enter Your Favorite Language : ")})
+
+print(dict)
+
+
+
+# # 19. What will be the length of following set s:
+
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add('20') # length of s after these operations?
+
+# print(s, len(s))
+
+
+
+# # 18. Can we have a set with 18 (int) and '18' (str) as a value in it?
+
+# s = {18, "18"}
+
+# print(s, type(s))
 
 
 # # 17. Write a program to input eight numbers from the user and display all the unique numbers
